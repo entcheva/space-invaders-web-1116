0a296422fb07dbb3c856b53d32b87f62f4d4b227
entcheva <christina.entcheva@gmail.com>
2017-01-23 22:04:46 -0500

    Done.

---
+++ spaceship.js
@@ -0,0 +1,27 @@
+class Spaceship {
+
+  constructor(name, crewMembers, phasers, shields) {
+    this.name = name
+    this.crewMembers = crewMembers
+    this.phasers = phasers
+    this.shields = shields
+    this.cloaked = false
+    this.warpDrive = 'disengaged'
+
+    if (crewMembers.length === 0) {
+      this.docked = true
+    } else {
+      this.docked = false
+    }
+
+    this.phasersCharge = 'uncharged'
+    this.crewUp()
+  }
+
+  crewUp() {
+    this.crewMembers.forEach(function(el) {
+      el.currentShip = this
+    }.bind(this))
+  }
+
+}
